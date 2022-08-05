7f93a52ea2d9ba6cc5c27e4f7a768fa7284ee48c
ronsun <ron.sun@hotmail.com.tw>
2022-08-06 01:34:14 +0800

    Add DecimalConstants

---
+++ MoreNet.Foundation/Constants/DecimalConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace MoreNet.Foundation.Constants
+{
+    /// <summary>
+    /// Constants for <see cref="decimal"/>.
+    /// </summary>
+    public static class DecimalConstants
+    {
+        /// <summary>
+        /// Represents the smallest positive <see cref="decimal"/> value that is greater than zero.
+        /// </summary>
+        public const decimal Epsilon = 1e-28m;
+    }
+}
